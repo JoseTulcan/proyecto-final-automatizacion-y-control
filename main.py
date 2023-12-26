01a80e4a2abf8c319469d4762cb2af1f317f3de2
JoseTulcan <90582244+JoseTulcan@users.noreply.github.com>
2023-12-26 23:15:58 +0000

    Actualización en proceso

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
-import streamlit as st 
+import streamlit as st
 
+#Configuración de diseño general de página(Fondo,texto)
+st.markdown(
+    """
+    <style>
+    .stApp {
+      background-color: #333333; 
+    }
+    .main h1,
+    .main h2,
+    .main h3, 
+    .main p,
+    .main li {
+      color: #fff;
+    }
+
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+#Titulo principal
 st.title('Proyecto final Automatización y control')
 
-st.text('Proyecto en construcción')
+st.header('Proyecto en construcción...')
+
+#Caja de habilitación de cámara
+show_camera = st.checkbox('Mostrar cámara') 
+
+picture = None
+
+if show_camera:
+    picture = st.camera_input("Tomar una foto")
+
+if picture:
+    st.image(picture)
+
